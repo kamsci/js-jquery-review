2f999da7bcc7852dd09da33116475bc4f26120aa
Krista Calderon <kamsci@gmail.com>
2016-07-20 15:06:38 -0700

    solutions for pluck and isPalindrome complete

--- js/getting-functional/isPalindrome.js
+++ js/getting-functional/isPalindrome.js
@@ -3,8 +3,28 @@
 
 // Example Input
 
-// var str = 'racecar';
+var str = 'abcccba';
 
 // Example Output
 
 // false
+
+
+function isPalindrome(string) {
+  newString = string.toLowerCase();
+
+  console.log("newString: ", newString);
+
+  for(var i = 0; i < newString.length; i++) {
+
+    var lastIdx = newString.length - (i + 1);
+
+    if (newString.charAt(i) !== newString.charAt(lastIdx)) {
+      return false;
+    };
+  };
+  return true;
+};
+
+var result = isPalindrome(str);
+
--- js/getting-functional/pluck.js
+++ js/getting-functional/pluck.js
@@ -3,12 +3,27 @@
 
 // Example Input
 
-// var albums = [
-//   { name: 'Gossamer', artist: 'Passion Pit' },
-//   { name: 'Sigh no more', artist: 'Mumford and Sons' },
-//   { name: 'Time to Pretend', artist: 'MGMT' }
-// ];
+var albums = [
+  { name: 'Gossamer', artist: 'Passion Pit' },
+  { name: 'Sigh no more', artist: 'Mumford and Sons' },
+  { name: 'Time to Pretend', artist: 'MGMT' }
+];
 
 // Example Output
 
 // [ 'Passion Pit', 'Mumford and Sons', 'MGMT' ]
+
+
+function pluck(array, key) {
+  resultArray = [];
+  for(var i = 0; i < array.length; i++) {
+    if (array[i][key]) {
+      resultArray.push(array[i][key]);
+    }
+  }
+  return resultArray;
+}
+
+var result = pluck(albums, "artist");
+
+console.log(result);
