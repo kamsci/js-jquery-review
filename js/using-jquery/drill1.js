3812447a86a70306f0b77d521543d54eaa8113b5
Krista Calderon <kamsci@gmail.com>
2016-07-20 19:04:53 -0700

    solution to jquery drill 1 and 2 complete

--- js/using-jquery/drill1.js
+++ js/using-jquery/drill1.js
@@ -1,8 +1,17 @@
 $(function(){
+  $('#ball').click(function () {
+    $('#ball').fadeOut();
 
+    width = Math.floor(Math.random() * 600);
+    height = Math.floor(Math.random() * 300);
+
+    setTimeout(function() {
+      $('#ball').css({"margin-left": width, "margin-top": height
+      });
+      $('#ball').fadeIn();
+    }, 1000);
+  });
 
     console.log('The dom is ready! (drill 1)');
-
-
 });
 
--- js/using-jquery/drill2.js
+++ js/using-jquery/drill2.js
@@ -1,12 +1,68 @@
 $(function(){
+  // var squarePosition = $('#square').position();
+  // var stagePosition = $('#stage').position();
 
 
-    console.log('The dom is ready! (drill 2)');
+  console.log('The dom is ready! (drill 2)');
+  // console.log("Top: " + squarePosition.top + " Left: " + squarePosition.left);
+  // console.log("Top: " + stagePosition.top + " Left: " + stagePosition.left);
 
-    $(document).on('keyup',function(event){
-        //output to console which key was pressed
-        console.log('The user pressed key: ',event.which);
-    });
 
-});
+  $(document).on('keyup',function(event){
+    //output to console which key was pressed
+    console.log('The user pressed key: ',event.which);
+    // prevent default arrow action
+    event.preventDefault();
+    // identify position of square
+    var squarePosition = $('#square').position();
+    var stagePosition = $('#stage').position();
+
+    var leftMove = squarePosition.left;
+    var topMove = squarePosition.top;
+
+    switch(event.which) {
+      case 37:
+        if (leftMove > 0) {
+          leftMove -= 100;
+          $('#square').css({ "left": leftMove });
+        } else {
+          alert("Too far left!");
+        };
+      break;
+
+      case 39:
+        if (leftMove < 800) {
+          leftMove += 100;
+          $('#square').css({ "left": leftMove });
+        } else {
+          alert("Too far right!");
+        };
+      break;
+
+      case 38:
+        if (topMove > 0) {
+          topMove -= 100;
+          $('#square').css({ "top": topMove });
+        } else {
+          alert("Too far up!");
+        };
+      break;
+
+      case 40:
+        if (topMove < 400) {
+          topMove += 100;
+          $('#square').css({ "top": topMove });
+        } else {
+          alert("Too far down!");
+        };
+      break;
+
+      case 83:
+        $('#square').effect('shake');
+      break;
+
+      default: return;
+    }
+  });
+}); // end main function
 
